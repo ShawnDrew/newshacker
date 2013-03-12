817be1a60755c225a9899d759b6ad033611238a5
Shawn Drew <shaztronics+github@gmail.com>
2013-03-12 20:41:00 +1100

    Update to support switch to HTTPS, Highlighting OP and noteable contributors, Show left border to highlight root comments

--- newshacker.js
+++ newshacker.js
@@ -1,3 +1,5 @@
-/*$(document).ready(function(){
-	$("table:first").attr('width', 500);
-});*/
+$(document).ready(function(){
+	// find OP and add a class to anchors
+	var op = $('.subtext:first a:first');
+	$('a[href="'+op.attr('href')+'"]').addClass('nh-op');
+});
